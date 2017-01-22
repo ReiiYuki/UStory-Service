e2be306240957a26de62e7aa8abb7eea7669459d
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-22 18:15:59 +0700

    Connect to MongoDB

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+
+let USTORY_DB_URI = 'mongodb://localhost:27017/ustory'
 
 let server = express()
 
-server.listen(4000,()=>{
-  console.log('Connect to Server at 4000 Successful')
+mongoose.connect(USTORY_DB_URI,(err)=>{
+  if (err) console.log(err)
+  else console.log('Connect to MongoDB at "'+USTORY_DB_URI+'" Successful')
+})
+server.listen(4000,(err)=>{
+  if (err) console.log(err)
+  else console.log('Starting Server at 4000...')
 })
